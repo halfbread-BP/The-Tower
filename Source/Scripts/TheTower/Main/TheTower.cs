14158a29b7e782474c638df46c8f79f3bc793630
halfbread-BP <oznerol.discord.gg@gmail.com>
2026-08-19 20:10:38 +0200

    Base

--- Source/Scripts/TheTower/Main/TheTower.cs
+++ Source/Scripts/TheTower/Main/TheTower.cs
@@ -1,15 +1,44 @@
-using Godot;
 using System;
+using Godot;
 
-public partial class TheTower : Node2D
+namespace Tt;
+
+/// <summary>
+/// Singleton that holds the game.
+/// </summary>
+public sealed partial class TheTower : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
+	public static TheTower instance { get; private set; }
+
+	public const string GAME_VERSION = "0.1.0";
+
+    public override void _EnterTree()
+    {
+        
+    }
+
 	public override void _Ready()
 	{
+		
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
+
+    public override void _PhysicsProcess(double delta)
+    {
+        
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        
+    }
+
+    public override void _ExitTree()
+    {
+        
+    }
 }
